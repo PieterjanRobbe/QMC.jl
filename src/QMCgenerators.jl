4d7adc789014251332365edf9ccdee06d6b29200
PieterjanRobbe <pieterjan.robbe@cs.kuleuven.be>
2016-08-04 20:24:59 +0200

    Fixed type-stability of lattice sequences

--- src/QMCgenerators.jl
+++ src/QMCgenerators.jl
@@ -6,44 +6,38 @@ abstract QMCgenerator{s}
 #
 
 # lattice sequence (32 bit)
-type LatSeq{s,U<:Unsigned} <: QMCgenerator{s}
-  	z::Vector{U} # generating vector
+type LatSeq{s,U<:Unsigned,V<:AbstractVector} <: QMCgenerator{s}
+  	z::V # generating vector (vector of U's, see http://docs.julialang.org/en/release-0.4/manual/performance-tips/#avoid-fields-with-abstract-containers)
 	nmax::U # max number of points
 	k::U # current point number
 	recipid::Float64
+
+	LatSeq(z::AbstractVector{U},nmax::U,k::U,recipid::Float64) = new(z,nmax,k,recipid)
 end
 
 # constructor
-function LatSeq{U<:Unsigned,N<:Integer}(z::Vector{U}, s::N; nmax::U = typemax(U))
+function LatSeq{U<:Unsigned,N<:Integer}(z::AbstractVector, s::N, nmax::U = typemax(U))
  	(s > 0 && s <= length(z)) || error("Can only generate $(nmax) points in $(length(z)) dimensions. 
  		Supply your own generating vector to increase.")
  	t = length(bin(typemax(U)))
- 	LatSeq{s,U}(z, nmax, zero(U), exp2(-t))
+ 	LatSeq{s,eltype(z),typeof(z)}(z, nmax, zero(U), exp2(-t)) :: LatSeq{s,U}
 end
 
 # default generating vectors
 function LatSeq(s::Integer)
+	(s > 0 && s <= 3600) || error("Cannot generate lattice points beyond dimension 3600, supply your own generating vector.")
 	if s <= 250
-		LatSeq(CKN_250_20, s, nmax = convert(UInt32,2^20))
-	elseif s <=3600
-		LatSeq(K_3600_32, s)
+		LatSeq(CKN_250_20, s, convert(UInt32,2^20))
 	else
-		error("Cannot generate lattice points beyond dimension 3600, supply your own generating vector.")
+		LatSeq(K_3600_32, s, typemax(UInt32))
 	end
-end
-
-# functions
-ndims{s}(l::LatSeq{s}) = s
-
-function show(io::IO, l::LatSeq)
-    print(io, "$(ndims(l))-dimensional lattice sequence")
 end
 
 # get the k'th point of the lattice sequence
 # a point has dimension (s,), where s is the dimenion of the lattice sequence
 function getPoint{s,N<:Integer}(l::LatSeq{s}, k::N)
 	k = convert(UInt32,k)
-	@assert k < l.nmax
+	#@assert k < l.nmax
 	phi_k = convert(Float64,reversebits(k $ k >>> 1)) 
 	x = zeros(s)
 	for i in 1:s
@@ -57,52 +51,52 @@ end
 #
 
 # interlaced polynomial lattice sequence
-type DigSeq{s,U<:Unsigned} <: QMCgenerator{s}
-  	C::Matrix{U} # generating matrix
+type DigSeq{s,U<:Unsigned,M<:AbstractMatrix} <: QMCgenerator{s}
+  	C::M # generating matrix
 	nmax::U # max number of points
 	k::U # current point number
-	cur::Vector{U} # previous point as integer
 	recipid::Float64
+
+	DigSeq(C::AbstractMatrix{U},nmax::U,k::U,recipid::Float64) = new(C,nmax,k,recipid)
 end
 
 # constructor
-function DigSeq{U<:Unsigned,N<:Integer}(C::Matrix{U}, s::N; nmax::U = typemax(U))
+function DigSeq{U<:Unsigned,N<:Integer}(C::AbstractMatrix, s::N, nmax::U = typemax(U))
  	(s > 0 && s <= size(C,1)) || error("Can only generate $(nmax) points in $(size(C,1)) dimensions. 
  		Supply your own generating matrix to increase.")
- 	C = reversebits(C)#[reversebits(C[i,j]) for i in 1:size(C,1), j in 1:size(C,2)]
+ 	C = reversebits(C)
  	t = maximum([ length(bin(C[1,i])) for i in 1:size(C,2) ])
- 	DigSeq{s,U}(C, nmax, zero(U), zeros(U,s), exp2(-t))
+ 	DigSeq{s,eltype(C),typeof(C)}(C, nmax, zero(U), exp2(-t))
 end
 
 # default generating matrices
 function DigSeq(s::Integer)
 	if s <= 32
-		DigSeq(N_32_32, s)
+		DigSeq(N_32_32, s, typemax(UInt32))
 	elseif s <=1111
-		DigSeq(JK_1111_32, s)
+		DigSeq(JK_1111_32, s, typemax(UInt32))
 	else
 		error("Cannot generate lattice points beyond dimension 1111, supply your own generating matrix.")
 	end
 end
 
-# functions
-ndims{s}(l::DigSeq{s}) = s
-
-function show(io::IO, l::DigSeq)
-    print(io, "$(ndims(l))-dimensional digital sequence")
-end
-
 # get the k'th point of the digital sequence
 # a point has dimension (s,), where s is the dimenion of the digital sequence
-function getPoint{s,U<:Unsigned}(d::DigSeq{s}, k::U)
+function getPoint{s,N<:Integer,U<:Unsigned}(d::DigSeq{s,U}, k::N)
 	k = convert(U,k)
 	k > 0 || return zeros(s) # return zero if k == 0
 	@assert k < d.nmax
-	c = length(bin(((k $ (k-1)) + 1) >> 1))
+	cur = zeros(U,s)
+	for i in 1:length(bits(k))
+		if ( k & (1 << (i - 1) ) ) != 0
+			for j in 1:s
+				cur[j] $= d.C[j,i]
+			end
+		end
+	end
 	x = zeros(s)
 	for i in 1:s
-		d.cur[i] = d.cur[i] $ d.C[i,c]
-		x[i] = d.recipid * d.cur[i]
+		x[i] = d.recipid * cur[i]
 	end
 	return x
 end
@@ -112,68 +106,76 @@ end
 #
 
 # random shift wrapper
-type RandWrapper{s,q,T}
+type RandWrapper{s,q,T,M<:AbstractMatrix}
 	generator::QMCgenerator{s} # underlying QMC generator
-	shifts::Matrix{T} # random shifts (s by q matrix)
-	shift_fcn::Function # function to shift this lattice sequence
+	shifts::M # random shifts (s by q matrix)
+
+	RandWrapper(generator::QMCgenerator{s},shifts::AbstractMatrix{T}) = new(generator,shifts)
 end
 
 # constructors
-function RandWrapper{s,U,N}(generator::LatSeq{s,U}, q::N)
+function RandWrapper{s,U,V,N}(generator::LatSeq{s,U,V}, q::N)
 	shifts = rand(s,q)
-	RandWrapper{s,q,Float64}(generator, shifts, lat_shift)
+	RandWrapper{s,q,eltype(shifts),typeof(shifts)}(generator, shifts)
 end
 
-function RandWrapper{s,U,N}(generator::DigSeq{s,U}, q::N)
-	shifts = convert(Array{U},floor(rand(s,q)*M))
-	RandWrapper{s,q,U}(generator, shifts, dig_shift)
+function RandWrapper{s,U,M,N}(generator::DigSeq{s,U,M}, q::N)
+	shifts = convert(Array{U},floor(rand(s,q)*typemax(U)))
+	RandWrapper{s,q,eltype(shifts),typeof(shifts)}(generator, shifts)
 end
 
-# functions
-ndims{s,q}(r::RandWrapper{s,q}) = s
-
-nshifts{s,q}(r::RandWrapper{s,q}) = q
-
-function show(io::IO, r::RandWrapper)
-    print(io, "$(ndims(r))-dimensional randomized sequence with $(nshifts(r)) shifts")
-end
-
-# get the k'th point of the randomized QMC generator
+# get the k'th point of the randomized lattice sequence
 # a point has dimension (s,q) with s the dimension of the QMC generator and q the number of shifts
-function getPoint{s,q,N<:Integer}(r::RandWrapper{s,q}, k::N)
+function getPoint{s,q,M,N<:Integer}(r::RandWrapper{s,q,Float64,M}, k::N)
 	x = getPoint(r.generator, k)
-	return r.shift_fcn(x,r.shifts)
+	return mod(x .+ r.shifts,1)
 end
 
-# function to shift the lattice sequence
-function lat_shift{T}(x::Vector{T},shifts::Matrix{T})
-	return mod(x .+ shifts,1)
-end
-
-# function to shift the digital sequence
-function dig_shift{T,U}(x::Vector{T},shifts::Matrix{U})
-	Ps = (shifts $ repmat(convert(Array{UInt32},floor(M*x)),1,size(shifts,2))) * recipid;
+# get the k'th point of the randomized digital sequence
+# a point has dimension (s,q) with s the dimension of the QMC generator and q the number of shifts
+function getPoint{s,q,U<:Unsigned,M,N<:Integer}(r::RandWrapper{s,q,U,M}, k::N)
+	x = getPoint(r.generator, k)
+	return (r.shifts $ repmat(convert(Array{UInt32},floor(typemax(U)*x)),1,size(r.shifts,2))) * r.generator.recipid;
 end
 
 #
 # GENERAL FUNCTIONS
 #
 
+# get number of dimensions
+ndims{s}(g::QMCgenerator{s}) = s
+
+ndims{s,q}(r::RandWrapper{s,q}) = s
+
+nshifts{s,q}(r::RandWrapper{s,q}) = q# functions
+
+function show(io::IO, l::DigSeq)
+    print(io, "$(ndims(l))-dimensional digital sequence")
+end
+
+function show(io::IO, r::RandWrapper)
+    print(io, "$(ndims(r))-dimensional randomized sequence with $(nshifts(r)) shifts")
+end
+
+function show(io::IO, l::LatSeq)
+    print(io, "$(ndims(l))-dimensional lattice sequence")
+end
+
 # get the next point of the QMC generator
 # a point has dimension (s,), where s is the dimenion of the QMC generator
-function next(generator::QMCgenerator)
-	x = getPoint(generator, generator.k)
-	generator.k += 1
+function next(g::QMCgenerator)
+	x = getPoint(g, g.k)
+	g.k += 1
 	return x
 end
 
 # get the next n points of the QMC generator
 # a point has dimension (s,), where s is the dimenion of the lattice sequence
 # all points are returned in a vector of size (n,)
-function next{N<:Integer}(generator::QMCgenerator, n::N)
+function next{N<:Integer}(g::QMCgenerator, n::N)
 	x = Vector{Float64}[] # turns out to be faster and more memeff than preallocating
 	for i in 1:n
-		@inbounds push!(x, next(generator))
+		@inbounds push!(x, next(g))
 	end
 	return x
 end
@@ -222,8 +224,5 @@ done(r::RandWrapper, r_::RandWrapper) = false # infinite loop
 
 # reverse bits of UInt type
 reversebits{U<:Unsigned}(n::U)=parse(U,reverse(bits(n)),2)
-@vectorize_1arg Unsigned reversebits
 
-# constant
-const recipid = exp2(-32)
-const M = 2^32
+@vectorize_1arg Unsigned reversebits # awesome!
