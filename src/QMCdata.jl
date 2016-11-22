a4704924262b419476321302dabc70da059bacd9
PieterjanRobbe <pieterjan.robbe@cs.kuleuven.be>
2016-11-22 09:55:24 +0100

    corrected QMCdata to vectorized format

--- src/QMCdata.jl
+++ src/QMCdata.jl
@@ -2,11 +2,11 @@
 PATH = LOAD_PATH[find([length(search(LOAD_PATH[i],"QMC")) for i in 1:length(LOAD_PATH)].!=0)][1]
 
 # the 250-dimensional generating vector of the Cools, Kuo and Nuyens paper in SIAM SISC for 2^20 points
-const CKN_250_20 = collect(readdlm(PATH*"/data/CKN_250_20.txt",UInt32))
+const CKN_250_20 = vec(readdlm(PATH*"/data/CKN_250_20.txt",UInt32))
 
 # 3600-dimensional generating vector with product weights 1/j^2 from 
 # http://web.maths.unsw.edu.au/~fkuo/lattice/
-const K_3600_32 = collect(readdlm(PATH*"/data/K_3600_32.txt",UInt32))
+const K_3600_32 = vec(readdlm(PATH*"/data/K_3600_32.txt",UInt32))
 
 # 32 dimensional generating matrix from the Niederreiter Xing directory on
 # https://people.cs.kuleuven.be/~dirk.nuyens/qmc-generators/nxmats/
